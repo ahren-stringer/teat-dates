cb4a92c271cafee094302fefaf2c18b16978f6a2
ahren-stringer <pavel12g@mail.ru>
2021-05-04 22:45:53 +0300

    deploy9

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Main from './Components/Main/Main';
 import { Route, withRouter } from 'react-router-dom';
 //  export let baseURL='http://localhost:8001/';
-export let baseURL='':
+export let baseURL='';
 function App() {
   return (
     <div className="App">
