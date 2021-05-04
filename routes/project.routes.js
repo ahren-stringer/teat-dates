59bdaf0415f1f654703251a3d7b8c0bb6175cc6c
ahren-stringer <pavel12g@mail.ru>
2021-05-04 22:34:38 +0300

    deploy9

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const db = require('../models/index.js');
 // import Connection from '../models/Connection.js';
-const Project = require('../models/project.js');
 const { Router } = express;
 const router = Router()
-const User = require('../models/user.js')
 
 router.post('/project', async (req, res) => {
     try {
